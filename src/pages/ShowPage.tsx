10e7ed2df26a2f51afb38e3d50de607f4c0bfd1b
Aftab Alam Khan <aftab.khan@pager.com>
2021-12-04 14:24:18 -0500

    chore: environment variable added

--- src/pages/ShowPage.tsx
+++ src/pages/ShowPage.tsx
@@ -7,20 +7,24 @@ interface Pokedex {
 const ShowPage = ({ pokemon }: Pokedex) => {
   return (
     <>
-      {pokemon.map((data: { name: string, url: string }, id: number) => {
+      {pokemon.filter((data: { url: string }) => {
         const rejex: any = data.url.match(/\d+/g);
-        return (
-          <div key={id}>
-            {(rejex[1] <= 151) ?
-              <div key={id} className="TitlePage">
-                <p key={id}>
-                  <Link to={`/pokemon/${rejex[1]}`} className="atag" key={rejex[1]}>{data.name}</Link>
-                </p>
-              </div>
-              : ''}
-          </div>
-        )
-      })}
+        const capValue = process.env.REACT_APP_CAP_VALUE    // capValue=151
+        if (!capValue) {
+          throw new Error("Please add environment(.env) variable 'REACT_APP_CAP_VALUE' value")
+        }
+        return (rejex[1] <= Number(capValue))
+      })
+        .map((data: { name: string, url: string }, id: number) => {
+          const rejex: any = data.url.match(/\d+/g);
+          return (
+            <div key={id} className="TitlePage">
+              <p key={id}>
+                <Link to={`/pokemon/${rejex[1]}`} className="atag" key={rejex[1]}>{data.name}</Link>
+              </p>
+            </div>
+          )
+        })}
     </>
   )
 };
